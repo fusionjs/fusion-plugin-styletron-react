95951e07a37e079cfec9b55c71f7e8be5230c586
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-01-01 23:05:24 -0800

    Move client init outside of middleware

--- src/browser.js
+++ src/browser.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Styletron from 'styletron-client';
 import {StyletronProvider} from 'styletron-react';
 
+const styleElements = document.getElementsByClassName(
+  '_styletron_hydrate_'
+);
+const styletron = new Styletron(styleElements);
+
 export default () => (ctx, next) => {
   if (ctx.element) {
-    const styleElements = document.getElementsByClassName(
-      '_styletron_hydrate_'
-    );
-    const styletron = new Styletron(styleElements);
-
     ctx.element = (
       <StyletronProvider styletron={styletron}>{ctx.element}</StyletronProvider>
     );
