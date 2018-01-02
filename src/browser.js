bd84fe1c18739b8ca55a4abd53dea9a27a496ecc
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-01-01 23:27:55 -0800

    Fix lint

--- src/browser.js
+++ src/browser.js
@@ -3,13 +3,16 @@ import React from 'react';
 import Styletron from 'styletron-client';
 import {StyletronProvider} from 'styletron-react';
 
-const styleElements = document.getElementsByClassName(
-  '_styletron_hydrate_'
-);
-const styletron = new Styletron(styleElements);
+let styletron;
 
 export default () => (ctx, next) => {
   if (ctx.element) {
+    if (!styletron) {
+      const styleElements = document.getElementsByClassName(
+        '_styletron_hydrate_'
+      );
+      styletron = new Styletron(styleElements);
+    }
     ctx.element = (
       <StyletronProvider styletron={styletron}>{ctx.element}</StyletronProvider>
     );
